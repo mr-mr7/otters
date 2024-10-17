c001bd6332672a340e87197b5480842bb695b524
mr_mr7 <mohamadrezanaderi39@gmail.com>
2024-10-18 00:08:19 +0330

    add task relation in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,32 +12,17 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -45,4 +30,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    // ============= Relation ==============
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+    // ============= Mutator ===============
+    // ============= Accessor ==============
+    // ============= Function ==============
+
 }
