a96976579e4f51660f4a8c9effa96abd921b9891
mr_mr7 <mohamadrezanaderi39@gmail.com>
2024-10-18 17:18:33 +0330

    add TaskCreated and TaskModified event

---
+++ app/Events/TaskCreated.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events;
+
+use App\Enums\TaskPriorityEnum;
+use App\Models\Task;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TaskCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Task $task)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('tasks'),
+        ];
+    }
+
+    public function broadcastQueue(): string
+    {
+        return match ($this->task->priority) {
+            TaskPriorityEnum::HIGH => 'high',
+            TaskPriorityEnum::MEDIUM => 'medium',
+            default => 'default',
+        };
+    }
+}
