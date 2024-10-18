9f157ace4dd9d368249731e53de421ad2412a272
mr_mr7 <mohamadrezanaderi39@gmail.com>
2024-10-18 22:23:37 +0330

    add Doc for TaskService

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -11,6 +11,10 @@ use Spatie\QueryBuilder\QueryBuilder;
 
 class TaskService
 {
+    /**
+     * List of tasks with filter like filter[priority]=90
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
     public function index()
     {
         $tasks = QueryBuilder::for(\App\Models\Task::class)
@@ -28,24 +32,49 @@ class TaskService
         return $tasks->paginate(\request()->per_page ?? 25);
     }
 
-    public function store($data)
+    /**
+     * Create new task
+     *
+     * @param array $data
+     * @return mixed
+     */
+    public function store(array $data): Task
     {
         return auth()->user()->tasks()->create($data);
     }
 
-    public function show(Task $task)
+    /**
+     * Show Task
+     *
+     * @param Task $task
+     * @return Task
+     */
+    public function show(Task $task): Task
     {
         return $task;
     }
 
-    public function update(Task|int $task, $data)
+    /**
+     * Update Task
+     *
+     * @param Task|int $task
+     * @param array $data
+     * @return Task
+     */
+    public function update(Task|int $task, array $data): Task
     {
         if (is_numeric($task)) $task = Task::query()->findOrFail($task);
         $task->update($data);
         return $task->fresh();
     }
 
-    public function destroy(Task $task)
+    /**
+     * Destroy Task
+     *
+     * @param Task $task
+     * @return bool
+     */
+    public function destroy(Task $task): bool
     {
         return $task->delete();
     }
